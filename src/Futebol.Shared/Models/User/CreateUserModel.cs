73c058f5d53859fcf203ae9ed2d7b951558fd287
Marcos Wellington <marcoswoc@gmail.com>
2023-03-19 12:00:35 -0300

    Ajustes Createuser, tela login e mainlayout.

--- src/Futebol.Shared/Models/User/CreateUserModel.cs
+++ src/Futebol.Shared/Models/User/CreateUserModel.cs
@@ -5,4 +5,8 @@ public class CreateUserModel : LoginModel
 {
     [Required(ErrorMessage = $"{nameof(UserName)} é obrigatório!")]
     public string UserName { get; set; }
+
+    [Required(ErrorMessage = $"{nameof(ConfirmPassword)} é obrigatório!")]
+    [Compare($"{nameof(Password)}", ErrorMessage = $"{nameof(ConfirmPassword)} não é o mesmo que {nameof(Password)}")]
+    public string ConfirmPassword { get; set; }
 }
